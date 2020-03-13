ae7df6af8729c16338025be4dc14dcbd29a682c1
Ernani N. da Costa <Ernanidacosta@users.noreply.github.com>
2020-03-13 11:44:07 -0300

    Deleted

--- modulo01/aula1/calculadora_aula1.py
+++
@@ -1,6 +0,0 @@
-nome = (input('Digite o seu nome:'))
-altura = float(input('Digite sua altura:'))
-peso = input('Digite seu peso:')
-
-imc = (altura*altura)/float(peso)
-print('{} - {} - {} - {:.2f}'.format(nome, altura, peso, imc))
